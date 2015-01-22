562f8d673c06c9f45daee68f75e98e1a286dc166
wojciech-szymanski <woszymanski@gmail.com>
2015-01-22 23:45:24 +0000

    Added automated web socket refresh when connection closed due to server restart

--- ng/websockets.js
+++ ng/websockets.js
@@ -1,15 +1,17 @@
 angular.module("app")
-	.run(function ($rootScope) {
-		var url = "ws://localhost:3000";
-		var connection = new WebSocket(url);
+	.run(function ($rootScope, $timeout) {
+		(function connect() {
+			var url = "ws://localhost:3000";
+			var connection = new WebSocket(url);
 
-		connection.onopen = function () {
-			console.log("WebSocket connected");
-		}
-
-		connection.onmessage = function (e) {
-			console.log(e);
-			var payload = JSON.parse(e.data);
-			$rootScope.$broadcast("ws:" + payload.topic, payload.data);
-		}
+			connection.onclose = function (e) {
+				console.log("WebSocket closed");
+				$timeout(connect, 10 * 1000);
+			}
+			connection.onmessage = function (e) {
+				console.log(e);
+				var payload = JSON.parse(e.data);
+				$rootScope.$broadcast("ws:" + payload.topic, payload.data);
+			}
+		})();
 	})
